78e25200d6c66f321277f47139d25a327d03f69d
Kieran Hanrahan <kieran@knhn.me>
2018-07-11 23:57:04 -0400

    Move script inside of def main() function
    Pylint was giving lots of overwriting variables from outer scope
    warnings so moved global part of the script inside of a main() function.

--- pyslater.py
+++ pyslater.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
+"""
+Docstring to come.
+"""
+
 import argparse
 import csv
 
 def read_csv_file(filename):
-    """Returns a list of data from a csv file passed to it.
+    """Returns a tuple of list data from a csv file passed to it.
     Prints any exceptions from reading the file."""
 
     try:
         with open(filename, 'rU') as open_file:
             rows = csv.reader(open_file)
-            return list(rows)
+            return tuple(rows)
     except Exception as ex:
         print ex
 
@@ -42,25 +46,39 @@ def tidy_text():
 
     pass
 
-if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(description="""generates .ttg files using a
-        template TTG file and CSV full of data to fill in fields""")
-    PARSER.add_argument("ttg_template", help="""path of the template TTG file""")
-    PARSER.add_argument("csv_file", help="""path of the CSV file""")
-    PARSER.add_argument("-u", "--underscores", action="store_true",
-            help="""replace spaces and illegal characters in output filenames""")
-    ARGS = PARSER.parse_args()
+def main():
+    """Docstring to be."""
 
-    print ARGS.csv_file
-    print ARGS.ttg_template
+    parser = argparse.ArgumentParser(description="""generates .ttg files using a
+        template TTG file and CSV full of data to fill in fields""")
+    parser.add_argument("ttg_template", help="""path of the template TTG file""")
+    parser.add_argument("csv_file", help="""path of the CSV file""")
+    parser.add_argument("-u", "--underscores", action="store_true",
+                        help="""replace spaces and illegal characters in output filenames""")
+    args = parser.parse_args()
+
+    print args.csv_file
+    print args.ttg_template
 
     #gather keywords
+    ttg_file_list = read_ttg_file(args.ttg_template)
+    ttg_keywords = find_ttg_keywords(ttg_file_list)
+    keywords = [(index, convert_from_ttg_text(raw_string)) for index, raw_string in ttg_keywords]
 
-    TTG_FILE_LIST = read_ttg_file(ARGS.ttg_template)
-    TTG_KEYWORDS = find_ttg_keywords(TTG_FILE_LIST)
-    KEYWORDS = [(a, convert_from_ttg_text(b)) for a, b in TTG_KEYWORDS]
+    #sort out csv
+    rows = read_csv_file(args.csv_file)
+    print '\n'.join(str(i) for i in rows)
 
     #start writing out ttgs
+    for i, line in enumerate(rows):
+        #skip header rowd
+        if i == 0:
+            pass
+        else:
+            #later add a filename formatting function
+            filename = "_".join([spot_name, duration, tidy_text(title)])
+            with open(filename, 'a') as the_file:
+                the_file.write(line)
 
-    ROWS = read_csv_file(ARGS.csv_file)
-    print '\n'.join(str(i) for i in ROWS)
+if __name__ == "__main__":
+    main()
